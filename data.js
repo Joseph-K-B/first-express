0a9aab48a24e68cc915eda2b2b4854643933c548
Joseph-K-B <303josephb@gmail.com>
2021-08-10 16:09:51 -0700

    added to dataset

--- data.js
+++ data.js
@@ -2,34 +2,87 @@ const data = [
     {
         id:1,
         key: 'c-major',
-        chord: 'C-major'
+        chord: 'C-major',
+        major: true,
+        class: 'primary'
     },
     {
-        id:1,
+        id:2,
         key: 'c-major',
-        chord: 'D-minor'
+        chord: 'D-minor',
+        major: false,
+        class: 'primary'
     },
     {
-        id:1,
+        id:3,
         key: 'c-major',
-        chord: 'E-minor'
+        chord: 'E-minor',
+        major: false,
+        class: 'primary'
     },
     {
-        id:1,
+        id:4,
         key: 'c-major',
-        chord: 'F-major'
+        chord: 'F-major',
+        major: true,
+        class: 'primary'
     },
     {
-        id:1,
+        id:5,
         key: 'c-major',
-        chord: 'G-major'
+        chord: 'G-major',
+        major: true,
+        class: 'primary'
     },
     {
-        id:1,
+        id:6,
         key: 'c-major',
-        chord: 'A-minor'
+        chord: 'A-minor',
+        major: false,
+        class: 'primary'
     },
-    
+    {
+        id:7,
+        key: 'c-major',
+        chord: 'C-major7',
+        major: true,
+        class: 'secondary'
+    },
+    {
+        id:8,
+        key: 'c-major',
+        chord: 'D-minor7',
+        major: false,
+        class: 'secondary'
+    },
+    {
+        id:9,
+        key: 'c-major',
+        chord: 'E-minor7',
+        major: false,
+        class: 'secondary'
+    },
+    {
+        id:10,
+        key: 'c-major',
+        chord: 'F-major7',
+        major: true,
+        class: 'secondary'
+    },
+    {
+        id:11,
+        key: 'c-major',
+        chord: 'G-dominant7',
+        major: false,
+        class: 'secondary'
+    },
+    {
+        id:12,
+        key: 'c-major',
+        chord: 'A-minor7',
+        major: false,
+        class: 'secondary'
+    },
 ]
 
 module.exports = data
